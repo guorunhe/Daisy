4a3ec4cd1fdb2296e6fab56300ef37906637196d
guorh <guorh@yonyou.com>
2017-11-12 02:09:47 +0800

    PHPExcel和测试前端效果.

---
+++ app/home/controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+  * @author guorunhe<guorunhe@foxmail.com>
+  * @date 17/11/12
+  */
+namespace home\controller;
+
+use core\Controller;
+/**
+  * index 控制器.
+  */
+class IndexController extends Controller
+{
+    public function index()
+    {
+        $this->assign('name', 'Daisy---Admin');
+        $this->display();
+    }
+}
